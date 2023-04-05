503719714f6212f1634ac75c3153bd0cd06dbd52
Frosty63101 <92688985+Frosty63101@users.noreply.github.com>
2023-04-05 10:38:05 -0400

    woohoo

--- AP Computer Science PT/AP Computer Science PT.cpp
+++ AP Computer Science PT/AP Computer Science PT.cpp
@@ -6,12 +6,12 @@ using namespace std;
     std::string longAdd(std::string a, std::string b);
     std::string longSubtract(std::string a, std::string b);
     std::string longMultiply(std::string a, std::string b);
-    //std::string longDivide(std::string a, std::string b);
+    std::string functionCall(std::string strOne, std::string strTwo, std::string opp);
 
 int main() {
 	string strOne, strTwo, out, opp, inputR;
     while (true) {
-        std::cout << "this program can add subtract multiply and divide numbers " <<
+        std::cout << "this program can add, subtract and multiply integers " <<
             "both smaller and larger than an integer can store" << endl << "using strings and arrays" << endl << endl;
         system("PAUSE");
         system("CLS");
@@ -23,25 +23,10 @@ int main() {
         std::cout << "enter string 2" << endl;
         cin >> strTwo;
 
-        if (opp == "+") {
-            out = longAdd(strOne, strTwo);
-        }
-        else if (opp == "-") {
-            out = longSubtract(strOne, strTwo);
-        }
-        else if (opp == "*") {
-            out = longMultiply(strOne, strTwo);
-        }
-        /*
-        else if (opp == "/") {
-            out = longDivide(strOne, strTwo);
-        }
-        */
-        else {
-            std::cout << "opperator " << opp << " is not a valid opperator" << endl;
-        }
+        out = functionCall(strOne, strTwo, opp);
 
         std::cout << strOne << " " << opp << " " << strTwo << " = " << out << endl;
+
         bool breakTest = false;
 
         while (true) {
@@ -51,7 +36,10 @@ int main() {
                 breakTest = true;
                 break;
             }
-            else if (inputR != "yes" && inputR != "no") {
+            else if (inputR == "yes") {
+                break;
+            }
+            else {
                 std::cout << "try again";
             }
         }
@@ -63,6 +51,29 @@ int main() {
 }
 
 
+
+std::string functionCall(std::string strOne, std::string strTwo, std::string opp) {
+    std::string out;
+
+    if (opp == "+") {
+        out = longAdd(strOne, strTwo);
+    }
+
+    else if (opp == "-") {
+        out = longSubtract(strOne, strTwo);
+    }
+
+    else if (opp == "*") {
+        out = longMultiply(strOne, strTwo);
+    }
+
+    else {
+        std::cout << "operator " << opp << " is not valid";
+        return "ERROR";
+    }
+
+    return out;
+}
 
 
 
@@ -93,7 +104,9 @@ std::string longAdd(std::string a, std::string b) {
         ss << maxL;
         ss >> tStr;
         err += tStr;
-        return err;
+        err += " digits long";
+        std::cout << err << endl;
+        return "ERROR";
     }
     if (bLength > maxL) {//return error if b is of greater length than the length of the array
         err = "string 2 is greater than ";
@@ -101,7 +114,9 @@ std::string longAdd(std::string a, std::string b) {
         ss << maxL;
         ss >> tStr;
         err += tStr;
-        return err;
+        err += " digits long";
+        std::cout << err << endl;
+        return "ERROR";
     }
 
     for (int i = aLength; i > 0; i--) {//input a into arr
@@ -148,7 +163,6 @@ std::string longAdd(std::string a, std::string b) {
 
 
 
-
 std::string longSubtract(std::string a, std::string b) {
     int arrA[100]{}, arrB[100]{}, arrF[100]{}, rollover, start{}, aLength = 0, bLength = 0, maxL;
     bool testforzero = false;
@@ -176,7 +190,9 @@ std::string longSubtract(std::string a, std::string b) {
         ss << maxL;
         ss >> tStr;
         err += tStr;
-        return err;
+        err += " digits long";
+        std::cout << err << endl;
+        return "ERROR";
     }
     if (bLength > maxL) {//return error if b is of greater length than the length of the array
         err = "string 2 is greater than ";
@@ -184,7 +200,9 @@ std::string longSubtract(std::string a, std::string b) {
         ss << maxL;
         ss >> tStr;
         err += tStr;
-        return err;
+        err += " digits long";
+        std::cout << err << endl;
+        return "ERROR";
     }
 
     for (int i = aLength; i > 0; i--) {//input a into arr
@@ -202,16 +220,17 @@ std::string longSubtract(std::string a, std::string b) {
     for (int i = (maxL - 1); i >= 0; i--) {//math functionality
         bool breaktest = true;
         if (arrA[i] < arrB[i]) {//borrow function
-            for (int n = 1; n <= (i - aLength) && breaktest; n++) {
+            for (int n = 0; n <= (i - aLength) && breaktest; n++) {
                 if (arrA[i - n] != 0) {
                     arrA[i - n] = arrA[i - n] - 1;
                     for (int l = 1; l <= n; l++) {
-                        breaktest = false;
-                        if (n = l) {
+                        if (n == l) {
                             arrA[i] = arrA[i] + 10;
+                            breaktest = false;
                         }
                         else {
-                            arrA[(i - n) + l] = arrA[(i - n) + l] + 9;
+                            arrA[(i - n) + l] = arrA[(i - n) + l] + 10;
+                            arrA[(i - n) + l] = arrA[(i - n) + l] - 1;
                         }
                     }
                 }
@@ -242,7 +261,6 @@ std::string longSubtract(std::string a, std::string b) {
 
 
 
-
 std::string longMultiply(std::string a, std::string b) {
     int arrA[100]{}, arrB[100]{}, arrF[2 * 100]{}, rollover, start{}, aLength = 0, bLength = 0, maxL;
     bool testforzero = false;
@@ -270,7 +288,9 @@ std::string longMultiply(std::string a, std::string b) {
         ss << maxL;
         ss >> tStr;
         err += tStr;
-        return err;
+        err += " digits long";
+        std::cout << err << endl;
+        return "ERROR";
     }
     if (bLength > maxL) {//return error if b is of greater length than the length of the array
         err = "string 2 is greater than ";
@@ -278,32 +298,31 @@ std::string longMultiply(std::string a, std::string b) {
         ss << maxL;
         ss >> tStr;
         err += tStr;
-        return err;
+        err += " digits long";
+        std::cout << err << endl;
+        return "ERROR";
     }
 
-    for (int i = aLength; i > 0; i--) {//input a into arr
+    for (int i = aLength; i >= 1; i--) {//input a into arr
         std::stringstream ss;
         ss << a[i - 1];
         ss >> arrA[((maxL - 1) - aLength) + i];
     }
 
-    for (int i = bLength; i > 0; i--) {//input b into arr
+    for (int i = bLength; i >= 1; i--) {//input b into arr
         std::stringstream ss;
         ss << b[i - 1];
         ss >> arrB[((maxL - 1) - bLength) + i];
     }
-    for (int i = (maxL - 1); i >= 0; i--) {//math functionality
-            if (aLength >= bLength) {
-                for (int n = maxL - 1; n >= (maxL - 1) - aLength; n--) {
-                    arrF[n] = (arrB[i] * arrA[n]) + arrF[n];
-                }
+
+    for (int i = (maxL * 2) - 1; i >= 0; i--) {//math functionality
+        for (int n = maxL - 1; n >= 0; n--) {
+            if ((i - n) >= 0 && (i - n) < maxL) {
+                arrF[i] += arrA[i - n] * arrB[n];
             }
-            else if (bLength > aLength) {
-                for (int n = maxL - 1; n >= (maxL - 1) - bLength; i--) {
-                    arrF[n] = (arrA[i] * arrB[n]) + arrF[n];
-                }
-            }
+        }
     }
+
     for (int i = ((maxL * 2) - 1); i >= 0; i--) {//math functionality
         if (arrF[i] > 9) {
             rollover = arrF[i] / 10;
@@ -311,15 +330,15 @@ std::string longMultiply(std::string a, std::string b) {
             arrF[i] = arrF[i] - (rollover * 10);
         }
     }
-
-    for (int i = 0; i < maxL && testforzero == false; i++) {//find the index of the start of the output
+    
+    for (int i = 0; i < 2 * maxL && testforzero == false; i++) {//find the index of the start of the output
         if (arrF[i] != 0) {
             testforzero = true;
             start = i;
         }
     }
 
-    for (int i = start; i < maxL; i++) {//transfer output from arr to str
+    for (int i = start; i < 2 * maxL; i++) {//transfer output from arr to str
         std::stringstream ss;
         std::string n;
         ss << arrF[i];
@@ -327,15 +346,10 @@ std::string longMultiply(std::string a, std::string b) {
         F += n;
     }
 
-    if (testforzero == true) {//return
+    if (testforzero == true) {
         return F;
     }
+    else {
+        return "ERROR";
+    }
 }
-
-
-
-/*
-std::string longDivide(std::string a, std::string b) {
-
-}
-*/
